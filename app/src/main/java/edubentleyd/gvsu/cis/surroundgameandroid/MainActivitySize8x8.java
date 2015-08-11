f65c010bf7b370b56de285281115dcf58e4acf32
Derrick Bentley <bentleyd@mail.gvsu.edu>
2015-08-10 23:54:06 -0400

    end of day
    working on gui

--- app/src/main/java/edubentleyd/gvsu/cis/surroundgameandroid/MainActivitySize8x8.java
+++ app/src/main/java/edubentleyd/gvsu/cis/surroundgameandroid/MainActivitySize8x8.java
@@ -4,26 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+public class MainActivitySize8x8 extends AppCompatActivity {
 
-    /** is the starting player */
-    int startPlayer;
-    /** is the number of players in the game. */
-    int players;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_activity_size8x8);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity_size8x8, menu);
         return true;
     }
 
@@ -40,10 +34,5 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-
     }
 }
